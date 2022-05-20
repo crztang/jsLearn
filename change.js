ab5ca40db171d199858029ab673b66f6e7f34e58
Tang <39229951+crztang@users.noreply.github.com>
2022-05-20 11:01:57 +0800

    create new file

---
+++ change.js
@@ -0,0 +1,5 @@
+let ele = '<head>';
+let eleReplace = '<head><link rel="stylesheet" href="https://limbopro.com/CSS/Adblock4limbo.user.css" type="text/css" /><script type="text/javascript" async="async" src="https://limbopro.com/Adguard/Adblock4limbo.user.js"></script>'
+let body = $response.body // 获取本次请求的响应体/返回体，此名为 body 的变量非 $done({ body }) 的 body；
+    .replace(ele, eleReplace) // 同 JavaScript replace()；
+$done({ body }); // 在代理工具环境下，使用 $done({}); 表示不对该请求进行修改；$done({ body }); 则意味着使用该 body 覆盖原来的响应 body；
