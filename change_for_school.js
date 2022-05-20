01613de91084fde23f15a12261cebaf4b5609b6b
Tang <39229951+crztang@users.noreply.github.com>
2022-05-20 14:37:03 +0800

    1

--- change_for_school.js
+++ change_for_school.js
@@ -18,12 +18,12 @@
 
     //var window = unsafeWindow
     //var url = location.pathname
-    
-    $(document).ready(function(){
-        alert("测试啊");
-        $("p:contains(学生学号)").css("text-decoration", "underline");
-        document.getElementsByClassName("hc-vaccin").innerHTML="test"
-        document.getElementsByClassName("school").innerHTML="test"
-        $(".hc-city").css("text-decoration", "underline");
+    alert("测试啊");
+    // $(document).ready(function(){
+        
+    //     $("p:contains(学生学号)").css("text-decoration", "underline");
+    //     document.getElementsByClassName("hc-vaccin").innerHTML="test"
+    //     document.getElementsByClassName("school").innerHTML="test"
+    //     $(".hc-city").css("text-decoration", "underline");
       
-     });
+    //  });
