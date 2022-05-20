67e09e8f2385a2f4d06c436b0a9712cf462544ef
Tang <39229951+crztang@users.noreply.github.com>
2022-05-20 14:47:51 +0800

    1

--- change.js
+++ change.js
@@ -1,5 +1,12 @@
+// let ele = '<head>';
+// let eleReplace = '<head><script type="text/javascript" async="async" src="https://raw.githubusercontent.com/crztang/jsLearn/main/change_for_school.js"></script>'
+// let body = $response.body.replace(ele, eleReplace)              // 获取本次请求的响应体/返回体，此名为 body 的变量非 $done({ body }) 的 body；
+//      // 同 JavaScript replace()；
+// $done({ body }); // 在代理工具环境下，使用 $done({}); 表示不对该请求进行修改；$done({ body }); 则意味着使用该 body 覆盖原来的响应 body；
+
+
 let ele = '<head>';
-let eleReplace = '<head><script type="text/javascript" async="async" src="https://raw.githubusercontent.com/crztang/jsLearn/main/change_for_school.js"></script>'
+let eleReplace = '<head><script alert("测试啊"); </script>'
 let body = $response.body.replace(ele, eleReplace)              // 获取本次请求的响应体/返回体，此名为 body 的变量非 $done({ body }) 的 body；
      // 同 JavaScript replace()；
 $done({ body }); // 在代理工具环境下，使用 $done({}); 表示不对该请求进行修改；$done({ body }); 则意味着使用该 body 覆盖原来的响应 body；
