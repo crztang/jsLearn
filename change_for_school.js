44c563167b65bc5d59a35bd59c4118a3c71d4b2a
Tang <39229951+crztang@users.noreply.github.com>
2022-05-20 22:34:42 +0800

    g

--- change_for_school.js
+++ change_for_school.js
@@ -15,22 +15,22 @@
     // ==/UserScript==
 
 
-    function myFunction()
-{
-    alert("测试啊");
-    //document.getElementById("demo").innerHTML="我的第一个 JavaScript 函数";
-}
+//     function myFunction()
+// {
+//     alert("测试啊");
+//     //document.getElementById("demo").innerHTML="我的第一个 JavaScript 函数";
+// }
 
 
 
     //var window = unsafeWindow
     //var url = location.pathname
     
-    // $(document).ready(function(){
-        
-    //     $("p:contains(学生学号)").css("text-decoration", "underline");
-    //     document.getElementsByClassName("hc-vaccin").innerHTML="test"
-    //     document.getElementsByClassName("school").innerHTML="test"
-    //     $(".hc-city").css("text-decoration", "underline");
+    $(document).ready(function(){
+        alert("测试sghhjg啊");
+        // $("p:contains(学生学号)").css("text-decoration", "underline");
+        // document.getElementsByClassName("hc-vaccin").innerHTML="test"
+        // document.getElementsByClassName("school").innerHTML="test"
+        // $(".hc-city").css("text-decoration", "underline");
       
-    //  });
+     });
