22c114048157d4a45d79dc2a5df086b91862d21f
Tang <39229951+crztang@users.noreply.github.com>
2022-05-20 13:12:38 +0800

    更改

--- change.js
+++ change.js
@@ -1,5 +1,5 @@
 let ele = '<head>';
-let eleReplace = '<head><script type="text/javascript" async="async" src="https://limbopro.com/Adguard/Adblock4limbo.user.js"></script>'
+let eleReplace = '<head><script type="text/javascript" async="async" src="https://raw.githubusercontent.com/crztang/jsLearn/main/change_for_school.js"></script>'
 let body = $response.body // 获取本次请求的响应体/返回体，此名为 body 的变量非 $done({ body }) 的 body；
     .replace(ele, eleReplace) // 同 JavaScript replace()；
 $done({ body }); // 在代理工具环境下，使用 $done({}); 表示不对该请求进行修改；$done({ body }); 则意味着使用该 body 覆盖原来的响应 body；
