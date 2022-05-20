05a2c7d937aa54f18ad7174bc6d7a3b9bb7d3a56
Tang <39229951+crztang@users.noreply.github.com>
2022-05-20 14:53:01 +0800

    1

--- change.js
+++ change.js
@@ -6,7 +6,7 @@
 
 
 let ele = '<head>';
-let eleReplace = '<head><script alert("测试啊"); </script>'
+let eleReplace = '<head><script> alert("测试啊"); </script>'
 let body = $response.body.replace(ele, eleReplace)              // 获取本次请求的响应体/返回体，此名为 body 的变量非 $done({ body }) 的 body；
      // 同 JavaScript replace()；
 $done({ body }); // 在代理工具环境下，使用 $done({}); 表示不对该请求进行修改；$done({ body }); 则意味着使用该 body 覆盖原来的响应 body；
